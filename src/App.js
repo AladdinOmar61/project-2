160069ca39ab24f94be44606728ca9aee8f13b65
Aladdin Omar <Aladdin.omar18@gmail.com>
2019-12-13 15:05:20 -0500

    rendered rick and morty images

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import CallApi from './components/CallApi';
+import CharList from './components/CharList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      charNames:[]
+    }
+  }
+
+  clickForChars = async (e) => {
+    e.preventDefault();
+    const getChars = await CallApi();
+    this.setState({
+      charNames: getChars
+    })
+  }
+  
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Ricpic and Morty-Schmorty</h1>
+        <h2>Why the hell are you here?</h2>
+        <CharList charNames={this.state.charNames} onClick={this.clickForChars}/>
+      </div>
+    );
+  }
 }
 
 export default App;

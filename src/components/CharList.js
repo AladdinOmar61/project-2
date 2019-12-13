1be296a5bab8b6893fc96c7a176fd88799737e65
Aladdin Omar <Aladdin.omar18@gmail.com>
2019-12-13 15:22:40 -0500

    got working 3x3 grid

--- src/components/CharList.js
+++ src/components/CharList.js
@@ -3,11 +3,10 @@ import React from 'react';
 const CharList = (props) => {
     return (
         <div>
-            <button onClick={props.onClick}>Click for Chars</button>
+            <button className="roster-click" onClick={props.onClick}>Click for Chars</button>
             {props.charNames.map((char, index) =>
             <div className="roster" key={index}>
-                <p>{char.name}</p>
-                <p>{char.status}</p>
+                <p className="name">{char.name}</p>
                 <img className="image" src={char.image}/>
             </div>
         )}

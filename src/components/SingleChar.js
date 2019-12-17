550d61c3d1f62986a6195c1f879b94d1ecd1e241
Aladdin Omar <Aladdin.omar18@gmail.com>
2019-12-17 16:38:29 -0500

    got conditional rendering to work

--- src/components/SingleChar.js
+++ src/components/SingleChar.js
@@ -3,22 +3,23 @@ import React from 'react';
 function SingleChar(props) {
     return (
         <div>
-            <form onChange={props.onCharSubmit} onSubmit={async (e) => {
+            <form onSubmit={async (e) => {
                 e.preventDefault();
                 await props.onCharClick();
             }}>
-
-                <input className="search-bar" type="text" onChange={props.charSearch}></input>
+                <div className="search-n-click">
+                <input className="search-bar" type="text" placeholder="Pick a number between 1-493" onChange={props.charSearch}></input>
                 <button className="find-button" type="submit">Find Ric Pic</button>
-                <div className="char-info">
-                    <div>
-                        <img className="single-img" src={props.charImg.image} alt=""/>
+                </div>
+                {props.apiImgLoaded &&
+                    <div className="char-info">
+                        <img className="single-img" src={props.charImg.image} alt="" />
                         <p className="name-list">Name: {props.charImg.name}</p>
                         <p className="gender">Gender: {props.charImg.gender}</p>
                         <p className="status">Status: {props.charImg.status}</p>
                         <p className="species">Species: {props.charImg.species}</p>
                     </div>
-                </div>
+                    }
             </form>
         </div>
     )

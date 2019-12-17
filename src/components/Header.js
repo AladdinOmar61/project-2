9de02c0bf05976ea6a55f3200e91d70aaf71b6df
Aladdin Omar <Aladdin.omar18@gmail.com>
2019-12-17 16:37:39 -0500

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+            <header>
+                <h1 className="title">Ricpic and Morty-Schmorty</h1>
+                <h2 className="slogan">The Rick and Morty Website</h2>
+            </header>
+        </div>
+    )
+}
+
+export default Header;

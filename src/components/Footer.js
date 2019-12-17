8a2b5b2f531c6fd81d25db3fddd2484cbac4ffae
Aladdin Omar <Aladdin.omar18@gmail.com>
2019-12-17 16:37:16 -0500

    added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer () {
+    return (
+        <div>
+            <footer>
+                <p className="wubaa">Wubba Lubba Dub Dub®</p>
+                <audio controls src="WUBBALUBBADUBDUB SOUND EFFECT.mp3"></audio>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
+
+// file:///Users/aladdinomar/Downloads/WUBBALUBBADUBDUB%20SOUND%20EFFECT.mp3

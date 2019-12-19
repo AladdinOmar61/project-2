68930f4d8c1298122f1d0c0c4c1e020dca3d8ea7
Aladdin Omar <Aladdin.omar18@gmail.com>
2019-12-19 10:10:21 -0500

    added media query

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
     return (
         <div className="header">
             <header>
-                <h1 className="title">Ricpic and Morty-Schmorty</h1>
+                <h1 className="title">Ricpic and Morty Schmorty</h1>
                 <h2 className="slogan">The Rick and Morty Website</h2>
             </header>
         </div>

dc9b2efa36bac9082e09d70ee6c1ad8ae0d45b5b
Aladdin Omar <Aladdin.omar18@gmail.com>
2019-12-19 14:34:12 -0500

    rendered vote count

--- src/components/SingleChar.js
+++ src/components/SingleChar.js
@@ -8,18 +8,23 @@ function SingleChar(props) {
                 await props.onCharClick();
             }}>
                 <div className="search-n-click">
-                <input value={props.value} className="search-bar" type="text" placeholder="Pick a number between 1-493" onChange={props.charSearch} ></input>
-                <button onSubmit={props.onCharSubmit} className="find-button" type="submit">Find Ric Pic</button>
+                    <input value={props.value} className="search-bar" type="text" placeholder="Pick a number between 1-493" onChange={props.charSearch} ></input>
+                    <button onSubmit={props.onCharSubmit} className="find-button" type="submit">Find Ric Pic</button>
                 </div>
                 {props.apiImgLoaded &&
                     <div className="char-info">
                         <img className="single-img" src={props.charImg.image} alt="" />
+                        <button className="morty" onClick={props.voteUp}>Give this a morty</button>
+                        <button className="schmorty" onClick={props.voteDown}>Give this a schmorty</button>
+                        <p>{props.votes}</p>
                         <p className="name-list">Name: {props.charImg.name}</p>
                         <p className="gender">Gender: {props.charImg.gender}</p>
                         <p className="status">Status: {props.charImg.status}</p>
                         <p className="species">Species: {props.charImg.species}</p>
+                        <div className="ricpic-votes">
+                        </div>
                     </div>
-                    }
+                }
             </form>
         </div>
     )
